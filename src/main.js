cab2d7ba30ba8dc8e2e068c41bb004898bf71b4c
shin <kouokukou@yahoo.co.jp>
2019-11-12 23:08:13 +0900

    チャッ土をルート変更用

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import firebase from 'firebase'
+import Vue from 'vue/dist/vue.esm.js';
+import App from './App'
 
 Vue.config.productionTip = false
 
+// Initialize Firebase
+var firebaseConfig = {
+   apiKey: "AIzaSyBUa1jsVxnKzKwUYygpXrkAdcWag4p4dp0",
+   authDomain: "chatapp-e215f.firebaseapp.com",
+   databaseURL: "https://chatapp-e215f.firebaseio.com",
+   projectId: "chatapp-e215f",
+   storageBucket: "chatapp-e215f.appspot.com",
+   messagingSenderId: "613752163571",
+   appId: "1:613752163571:web:932b2a927bd180ce"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+/* eslint-disable no-new */
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
